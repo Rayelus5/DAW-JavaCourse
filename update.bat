311b8337acfb047bc5bc721aab976d872a19eddb
Rayelus <92628182+Rayelus5@users.noreply.github.com>
2024-09-30 11:23:47 +0200

    Delete update.bat

--- update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd %USERPROFILE%\Desktop\DAW\ASIGNATURAS\1-PROGRAMACION\JavaCourse\JavaCourse
-echo [COMMIT]:
-set /p commit=
-git status
-git add .
-git commit -m "%commit%"
-git push
-pause
