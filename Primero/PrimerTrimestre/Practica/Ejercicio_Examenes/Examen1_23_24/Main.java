d6b4018feed1408841e0f11ba119c0647a88e60c
rayelus5 <rayelusanom828@gmail.com>
2024-10-16 10:52:10 +0200

    16/10/24 - New Examns

---
+++ Primero/PrimerTrimestre/Practica/Ejercicio_Examenes/Examen1_23_24/Main.java
@@ -0,0 +1,89 @@
+package Primero.PrimerTrimestre.Practica.Ejercicio_Examenes.Examen1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totalTiendas = 0;
+        int totalProductos = 0;
+        int maxProductosCampania = 0;
+        float mayorDescuento = 0;
+        String productoMayorDescuento = "";
+        int productosQueBajanPrecio = 0;
+        float menorPrecioAntesBF = Float.MAX_VALUE;
+        String productoMenorPrecioAntesBF = "";
+
+        while (true) {
+            System.out.print("Ingrese el nombre de la tienda (o 'fin' para terminar): ");
+            String nombreTienda = scanner.nextLine();
+            if (nombreTienda.equals("fin")) {
+                break;
+            }
+
+            totalTiendas++;
+            System.out.print("¿Hace campaña para el Black Friday? (S/N): ");
+            char haceCampania = scanner.nextLine().charAt(0);
+
+            int productosEnEstaTienda = 0;
+
+            while (true) {
+                System.out.print("Ingrese el nombre del producto (o 'fin' para terminar): ");
+                String nombreProducto = scanner.nextLine();
+                if (nombreProducto.equals("fin")) {
+                    break;
+                }
+
+                System.out.print("Ingrese el precio normal del producto: ");
+                float precioNormal = Float.parseFloat(scanner.nextLine());
+                System.out.print("Ingrese el precio justo antes del Black Friday: ");
+                float precioAntesBF = Float.parseFloat(scanner.nextLine());
+                System.out.print("Ingrese el precio en el Black Friday: ");
+                float precioBlackFriday = Float.parseFloat(scanner.nextLine());
+
+                productosEnEstaTienda++;
+                totalProductos++;
+
+                // Verificar el producto con mayor descuento
+                float descuento = precioNormal - precioBlackFriday;
+                if (descuento > mayorDescuento) {
+                    mayorDescuento = descuento;
+                    productoMayorDescuento = nombreProducto;
+                }
+
+                // Contar cuántos productos realmente bajan de precio
+                if (precioBlackFriday < precioNormal) {
+                    productosQueBajanPrecio++;
+                }
+
+                // Producto con el menor precio antes del BF en tiendas sin campaña
+                if (haceCampania == 'N' && precioAntesBF < menorPrecioAntesBF) {
+                    menorPrecioAntesBF = precioAntesBF;
+                    productoMenorPrecioAntesBF = nombreProducto;
+                }
+            }
+
+            // Actualizar el máximo de productos en tiendas que hacen campaña
+            if (haceCampania == 'S' && productosEnEstaTienda > maxProductosCampania) {
+                maxProductosCampania = productosEnEstaTienda;
+            }
+        }
+
+        // Mostrar los resultados
+        System.out.println("\nResultados del estudio:");
+        if (totalTiendas > 0) {
+            System.out.printf("a) Número medio de productos estudiados por tienda: %.2f%n", (float) totalProductos / totalTiendas);
+        }
+        System.out.println("b) Número máximo de productos estudiados en tiendas que sí han hecho campaña de Black Friday: " + maxProductosCampania);
+        System.out.println("c) Nombre del producto que ha bajado más euros entre su precio normal y su precio en Black Friday: " + productoMayorDescuento);
+        System.out.println("d) Número de productos que han bajado de precio realmente en Black Friday con respecto a su precio normal: " + productosQueBajanPrecio);
+        if (!productoMenorPrecioAntesBF.isEmpty()) {
+            System.out.println("e) Nombre del producto con precio justo antes del Black Friday más barato de las tiendas que no hacen campaña en el Black Friday: " + productoMenorPrecioAntesBF);
+        } else {
+            System.out.println("e) No hay productos en tiendas que no hacen campaña.");
+        }
+
+        scanner.close();
+    }
+}
---
+++ Primero/PrimerTrimestre/Practica/Ejercicio_Examenes/Examen5_17_18/Material/ENTRADA.ahk
@@ -0,0 +1,221 @@
+﻿
+
+
+
+SetKeyDelay ,100
+
+^d::
+
+Send, Real Betis{enter}
+Send, 22{enter}
+Send, 4{enter}
+
+Send, Corral{enter}
+Send, 24{enter}
+Send, 3000.8{enter}
+Send, P{enter}
+Send, 7{enter}
+
+Send, Lasa{enter}
+Send, 23{enter}
+Send, 2500.1{enter}
+Send, F{enter}
+
+Send, Martínez{enter}
+Send, 27{enter}
+Send, 4000.5{enter}
+Send, L{enter}
+
+Send, Areta{enter}	
+Send, 26{enter}
+Send, 1000.1{enter}
+Send, D{enter}
+Send, 4{enter}
+
+Send, Málaga{enter}
+Send, 17{enter}
+Send, 6{enter}
+
+Send, Pachón{enter}
+Send, 23{enter}
+Send, 2500.1{enter}
+Send, F{enter}
+
+Send, Villanova{enter}
+Send, 24{enter}
+Send, 3800.8{enter}
+Send, P{enter}
+Send, 8{enter}
+
+Send, Isauro{enter}
+Send, 27{enter}
+Send, 4060.5{enter}
+Send, L{enter}
+Send, Machicha{enter}
+	
+Send, 25{enter}
+Send, 1200.1{enter}
+Send, D{enter}
+Send, 4{enter}
+
+Send, Benítez{enter}
+Send, 27{enter}
+Send, 4400.5{enter}
+Send, L{enter}
+
+Send, Mellado{enter}
+Send, 21{enter}
+Send, 1000.1{enter}
+Send, D{enter}
+Send, 3{enter}
+
+Send, Recreativo de Huelva{enter}
+Send, 19{enter}
+Send, 7{enter}
+
+Send, Rincón{enter}
+Send, 21{enter}
+Send, 1450.1{enter}
+Send, D{enter}
+Send, 1{enter}
+
+Send, Diarte{enter}
+Send, 25{enter}
+Send, 2300.1{enter}
+Send, D{enter}
+Send, 6{enter}
+
+Send, Cardeñosa{enter}
+Send, 29{enter}
+Send, 4060.5{enter}
+Send, L{enter}
+
+Send, Gordillo{enter}
+Send, 27{enter}
+Send, 4800.5{enter}
+Send, L{enter}
+
+Send, Canito{enter}
+Send, 24{enter}
+Send, 4900.5{enter}
+Send, L{enter}
+
+Send, Biosca{enter}	
+Send, 23{enter}
+Send, 2500.1{enter}
+Send, F{enter}
+
+Send, Esnaola{enter}
+Send, 24{enter}
+Send, 3800.8{enter}
+Send, P{enter}
+Send, 2{enter}
+
+Send, fin{enter}
+
+Return
+
+^e::
+
+Send, Primer{enter}
+Send, 19{enter}
+Send, 2{enter}
+
+Send, aaa{enter}
+Send, 22{enter}
+Send, 1450.1{enter}
+Send, L{enter}
+
+
+Send, bbb{enter}
+Send, 25{enter}
+Send, 2300.1{enter}
+Send, F{enter}
+
+
+Send, Ultimo{enter}
+Send, 22{enter}
+Send, 2{enter}
+
+Send, ccc{enter}
+Send, 24{enter}
+Send, 1450.1{enter}
+Send, F{enter}
+
+
+Send, ddd{enter}
+Send, 25{enter}
+Send, 2300.1{enter}
+Send, D{enter}
+Send, 6{enter}
+
+Send, fin{enter}
+
+Return
+
+^f::
+
+Send,1{enter}
+Send,2{enter}
+Send,3{enter}
+Send,6{enter}
+Send,3{enter}
+Send,9{enter}
+Send,2{enter}
+Send,3{enter}
+Send,4{enter}
+Send,5{enter}
+Send,1{enter}
+Send,8{enter}
+Send,7{enter}
+Send,6{enter}
+Send,7{enter}
+Send,8{enter}
+Send,4{enter}
+Send,3{enter}
+Send,2{enter}
+Send,5{enter}
+Send,6{enter}
+Send,7{enter}
+Send,8{enter}
+Send,9{enter}
+Send,10{enter}
+Send,11{enter}
+Send,2{enter}
+Send,4{enter}
+Send,5{enter}
+Send,3{enter}
+Send,-1{enter}
+
+
+Return
+
+^t::
+
+Send,9{enter}
+Send,8{enter}
+Send,7{enter}
+Send,6{enter}
+Send,5{enter}
+Send,4{enter}
+Send,3{enter}
+Send,2{enter}
+
+Send,-1{enter}
+
+
+Return
+
+
+
+^r::
+
+Send, Primer{enter}
+Send, 19{enter}
+Send, 1{enter}
+
+Send, aaa{enter}
+Send, 22{enter}
+Send, 1450.1{enter}
+Send, L{enter}
+Send, fin{enter}
